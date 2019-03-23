5d3b1bba6469b6f21fafa688cc92f96da8360346
Matthew <matthew.r.martin@me.com>
2019-03-23 10:35:49 -0700

    Initial commit.
    Signed-off-by: Matthew <matthew.r.martin@me.com>

---
+++ TA-Code.ino
@@ -0,0 +1,34 @@
+/**
+ * TRAUMA ALARMA MAIN CODE
+ * 
+ * ENGR-152 Product design project
+ * Cole Weidkamp, Garrett Cooper, Matthew Martin, Marco Scoma
+ * 
+ * Created 3/23/19 by Matthew
+ * 
+ * "Code does not come from winning. Your struggles develop your code. When you go through crashes and decide not to surrender, that is programming."
+ * "The worst thing I can be is a civil engineer. I hate that."
+ * "Windows is for babies. When you grow up you have to use Linux."
+ */
+
+
+
+/**
+ * MAIN CODE BLOC. DOES THE MAIN STUFF
+ */
+void setup() 
+{
+  
+
+}
+
+void loop() 
+{
+
+
+}
+
+
+/**
+ * SOVIET CODE BLOC. FUNCTION OWNED BY EVERYBODY, LIKE TRUE COMMUNISM.
+ */
